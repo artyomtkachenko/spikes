8f0433ed2fed72b5ce76299a243f7dc652c5b473
Tim <timtkachenko@nbnco.com.au>
2015-12-07 14:34:59 +1100

    Just tested vim plugin

--- rand.go
+++ rand.go
@@ -1,115 +1,115 @@
 package main
+
 import (
-  "fmt"
-  "math/rand"
-  "time"
-  "sort"
-  "encoding/json"
-  "strconv"
-  "runtime"
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"runtime"
+	"sort"
+	"strconv"
+	"time"
 )
 
-
-func getMax(max int, res map[int]int) int {
-  for k, v := range res {
-    fmt.Println(k,v)
-    if v == max {
-      return k
-    }
-  }
-  return 0
-}
+// func getMax(max int, res map[int]int) int {
+// 	if v == max {
+// 		return k
+// 	}
+// 	return 0
+// }
 
 func init() {
-  rand.Seed(time.Now().UnixNano())
+	rand.Seed(time.Now().UnixNano())
 }
 
 func getRandNumber(n int) int {
-  res := make(map[int]int) //will contain 36 numbers
-  for i := 0; i < 65535; i++ {
-    res[rand.Intn(n)] += 1
-  }
-  max := 0
-  maxKey := -1
-  for k, v := range res {
-    if v > max {
-      max = v
-      maxKey = k
-    }
-  }
-  return maxKey
+	res := make(map[int]int) //will contain N numbers
+	for i := 0; i < 25000000; i++ {
+		res[rand.Intn(n)]++
+	}
+	max := 0
+	maxKey := -1
+	for k, v := range res {
+		if v > max {
+			max = v
+			maxKey = k
+		}
+	}
+	return maxKey
 }
 
 func genTicket(n int, m int) map[string]string {
-  res := make(map[int]int) //will contain 6 numbers
-  for i := 1; i <= n ; i++ {
-    again:
-    max := getRandNumber(m)
-    val, ok := res[max]
-    if ok {
-      goto again // we do have it already
-    } else if val == 0 && ok {
-      goto again
-    } else if max == 0 {
-      goto again
-    } else {
-      res[max] = i
-    }
-  }
-  return convertToString(res)
+	res := make(map[int]int)
+	for i := 1; i <= n; i++ {
+	again:
+		max := getRandNumber(m)
+		val, ok := res[max]
+		if ok {
+			goto again // we do have it already
+		} else if val == 0 && ok {
+			goto again
+		} else if max == 0 {
+			goto again
+		} else {
+			res[max] = i
+		}
+	}
+	return convertToString(res)
 }
 
-func convertToString(m map[int]int) map[string]string  {
-  res := make(map[string]string)
-  for k, v := range m {
-    res[strconv.Itoa(k)] = strconv.Itoa(v)
-  }
-  return res
+func convertToString(m map[int]int) map[string]string {
+	res := make(map[string]string)
+	for k, v := range m {
+		res[strconv.Itoa(k)] = strconv.Itoa(v)
+	}
+	return res
 }
 
 func sortMapByValues(m map[int]int) {
-  arr := make([]int, 0)
-  for _, v := range m {
-    arr = append(arr, v)
-  }
-  sort.Ints(arr)
-  for _, v := range arr {
-    fmt.Println(v)
-  }
+	var arr []int
+	for _, v := range m {
+		arr = append(arr, v)
+	}
+	sort.Ints(arr)
+	for _, v := range arr {
+		fmt.Println(v)
+	}
 }
 
-func produce(n int,queue chan map[string]string) { // writes into the channel
-  for i := 0; i <= n; i++ {
-    queue <- genTicket(7, 45)
-  }
+func produce(n int, queue chan map[string]string, minor int, major int) { // writes into the channel
+	for i := 0; i <= n; i++ {
+		queue <- genTicket(minor, major)
+	}
 }
 
 func reduce(tasks int, queue chan map[string]string) { //reads from the channel
-  arr := make([]map[string]string, 0)
-  // for r := range queue {
-  for i := 0; i <= tasks; i++ {
-    res := <- queue
-    arr = append(arr, res)
-  }
+	var arr []map[string]string
+	// for r := range queue {
+	for i := 0; i <= tasks; i++ {
+		res := <-queue
+		arr = append(arr, res)
+	}
 
-  out, err := json.Marshal(arr)
-  if err != nil {
-    panic(err)
-  }
-  fmt.Println(string(out))
+	out, err := json.Marshal(arr)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(out))
 }
 
 func main() {
-  cores := runtime.NumCPU()
+	cores := runtime.NumCPU()
 
-  fmt.Printf("This machine has %d CPU cores. \n", cores)
-  runtime.GOMAXPROCS(cores)
+	fmt.Printf("This machine has %d CPU cores. \n", cores)
+	runtime.GOMAXPROCS(cores)
 
-  ticketsToGenerate := 2000
-  tasksPerCore := ticketsToGenerate / cores
-  queue := make(chan map[string]string, ticketsToGenerate)
-  for i := 0; i != cores; i++ {
-    go produce(tasksPerCore, queue)
-  }
-  reduce(ticketsToGenerate, queue)
+	ticketsToGenerate := 20
+	minorNumber := 6
+	majorNumber := 36
+
+	tasksPerCore := ticketsToGenerate / cores
+	queue := make(chan map[string]string, ticketsToGenerate)
+	for i := 0; i != cores; i++ {
+		go produce(tasksPerCore, queue, minorNumber, majorNumber)
+	}
+	reduce(ticketsToGenerate, queue)
 }
